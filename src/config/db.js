9b3bbd058c74df9980b9a8e350f61f7debf1099e
luciano.dev <mansillalucianodamian@gmail.com>
2026-06-26 10:21:39 -0300

    console para db

--- src/config/db.js
+++ src/config/db.js
@@ -1,8 +1,11 @@
 import mysql from 'mysql2/promise';
 import ENVIROMENT from './enviroment.config.js';
 
+console.log("DB_HOST:", ENVIROMENT.DB_HOST);
+console.log("DB_PORT:", ENVIROMENT.DB_PORT);
 console.log("DB_USER:", ENVIROMENT.DB_USER);
 console.log("DB_PASSWORD:", ENVIROMENT.DB_PASSWORD);
+console.log("DB_NAME:", ENVIROMENT.DB_NAME);
 
 export const pool = mysql.createPool({
   host: ENVIROMENT.DB_HOST,
