f44fd5753702af7b26caa5733dad81a4ff00ca64
luciano.dev <mansillalucianodamian@gmail.com>
2026-06-25 23:52:24 -0300

    cambios en db

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,13 @@
-// src/config/db.js
 import mysql from 'mysql2/promise';
+import ENVIROMENT from './enviroment.config.js';
 
-console.log("DB_USER:", process.env.DB_USER);
-console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
+console.log("DB_USER:", ENVIROMENT.DB_USER);
+console.log("DB_PASSWORD:", ENVIROMENT.DB_PASSWORD);
+
 export const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  host: ENVIROMENT.DB_HOST,
+  port: ENVIROMENT.DB_PORT,
+  user: ENVIROMENT.DB_USER,
+  password: ENVIROMENT.DB_PASSWORD,
+  database: ENVIROMENT.DB_NAME
 });
--- src/config/enviroment.config.js
+++ src/config/enviroment.config.js
@@ -1,21 +1,23 @@
-import dotenv from 'dotenv'
-// cargar las variables de entorno en process.env
-dotenv.config()
+import dotenv from 'dotenv';
+
+// Forzar la carga del .env correcto
+dotenv.config({ path: '/home/administrador/backend-reservas/.env' });
 
 const ENVIROMENT = {
-    GMAIL_PASSWORD: process.env.GMAIL_PASSWORD,
-    GMAIL_USER: process.env.GMAIL_USER,
-    PORT: process.env.PORT,
-    URL_FRONTEND: process.env.URL_FRONTEND,
-    JWT_SECRET: process.env.JWT_SECRET,
-    URL_BACKEND: process.env.URL_BACKEND,
+  GMAIL_PASSWORD: process.env.GMAIL_PASSWORD,
+  GMAIL_USER: process.env.GMAIL_USER,
+  PORT: process.env.PORT,
+  URL_FRONTEND: process.env.URL_FRONTEND,
+  JWT_SECRET: process.env.JWT_SECRET,
+  URL_BACKEND: process.env.URL_BACKEND,
 
-    // Configuración MySQL
-    DB_HOST: process.env.DB_HOST,
-    DB_PORT: process.env.DB_PORT,
-    DB_USER: process.env.DB_USER,
-    DB_PASSWORD: process.env.DB_PASSWORD,
-    DB_NAME: process.env.DB_NAME,
-}
+  // Configuración MySQL
+  DB_HOST: process.env.DB_HOST,
+  DB_PORT: process.env.DB_PORT,
+  DB_USER: process.env.DB_USER,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+  DB_NAME: process.env.DB_NAME,
+};
 
-export default ENVIROMENT
+export default ENVIROMENT;
+
